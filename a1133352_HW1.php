4393e3de4e14fe42ca0f7148932698fd609b4269
lunlun0830 <a1133352@mail.nuk.edu.tw>
2026-06-26 04:17:23 +0800

    Add files via upload

---
+++ a1133352_HW1.php
@@ -0,0 +1,66 @@
+<HTML>
+<HEAD>
+    <TITLE>夏令營報名單</TITLE>
+</HEAD>
+<BODY BGCOLOR="lightgreen">
+    
+    <CENTER>
+        <H1><FONT COLOR="DARKBLUE">夏日探索夏令營</FONT></H1>
+        <B><I>歡迎來到最有趣的夏令營！請填寫以下報名資訊：</I></B>
+        <HR>
+    </CENTER>
+
+    <P>報名須知：</P>
+    <UL>
+        <LI>請確實填寫學員聯絡資訊。</LI>
+        <LI>報名費用請於填表後三日內完成繳交。</LI>
+    </UL>
+
+    <CENTER>
+    <FORM ACTION="submit.php" METHOD="POST">
+        <TABLE BORDER="1" BGCOLOR="white">
+            <TR>
+                <TH COLSPAN="2">學員基本資料</TH>
+            </TR>
+            <TR>
+                <TD>學員姓名</TD>
+                <TD><INPUT TYPE="text" NAME="student_name"></TD>
+            </TR>
+            <TR>
+                <TD>性別</TD>
+                <TD>
+                    <INPUT TYPE="radio" NAME="gender" VALUE="male"> 男
+                    <INPUT TYPE="radio" NAME="gender" VALUE="female"> 女
+                </TD>
+            </TR>
+            <TR>
+                <TD>聯絡電話</TD>
+                <TD><INPUT TYPE="text" NAME="phone"></TD>
+            </TR>
+            <TR>
+                <TD>參加梯次</TD>
+                <TD>
+                    <SELECT NAME="camp_session">
+                        <OPTION VALUE="1">第一梯次 (7/1 - 7/5)</OPTION>
+                        <OPTION VALUE="2">第二梯次 (7/15 - 7/19)</OPTION>
+                    </SELECT>
+                </TD>
+            </TR>
+            <TR>
+                <TH COLSPAN="2">
+                    <INPUT TYPE="submit" VALUE="送出報名單">
+                    <INPUT TYPE="reset" VALUE="重新填寫">
+                </TH>
+            </TR>
+        </TABLE>
+    </FORM>
+    </CENTER>
+
+    <HR>
+
+    <CENTER>
+        <IMG SRC="https://thumbs.dreamstime.com/b/%E5%9C%A8%E5%A4%8F%E5%A4%A9%E6%97%A5%E5%87%BA%E7%9A%84%E6%9C%89%E9%9B%BE%E7%9A%84%E6%B9%96%E8%BE%B9%E4%B8%8E%E9%AB%98%E8%8D%89-123369293.jpg" ALT="湖邊日出夏令營圖片" style="max-width: 100%; height: auto;">
+    </CENTER>
+
+</BODY>
+</HTML>
